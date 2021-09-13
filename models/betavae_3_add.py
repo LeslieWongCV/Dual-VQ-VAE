dbd53c0c31cd6891e54ff3923ef8d10267f47b0a
Yushuo Wang <38121764+LeslieWongCV@users.noreply.github.com>
2021-09-13 11:27:23 +0800

    Add files via upload

---
+++ models/betavae_3_add.py
@@ -0,0 +1,99 @@
+import torch
+import torch.nn as nn
+#import torch.nn.functional as F
+import torch.nn.init as init
+from torch.autograd import Variable
+
+
+def reparametrize(mu, logvar):
+    std = logvar.div(2).exp()
+    eps = Variable(std.data.new(std.size()).normal_())
+    return mu + std*eps
+
+def kl_divergence(mu, logvar):
+    batch_size = mu.size(0)
+    assert batch_size != 0
+    if mu.data.ndimension() == 4:
+        mu = mu.view(mu.size(0), mu.size(1))
+    if logvar.data.ndimension() == 4:
+        logvar = logvar.view(logvar.size(0), logvar.size(1))
+
+    klds = -0.5*(1 + logvar - mu.pow(2) - logvar.exp())
+    total_kld = klds.sum(1).mean(0, True)
+    dimension_wise_kld = klds.mean(0)
+    mean_kld = klds.mean(1).mean(0, True)
+
+    return total_kld, dimension_wise_kld, mean_kld
+
+class View(nn.Module):
+    def __init__(self, size):
+        super(View, self).__init__()
+        self.size = size
+
+    def forward(self, tensor):
+        return tensor.view(self.size)
+
+
+class BetaVAE_H(nn.Module):
+    """Model proposed in original beta-VAE paper(Higgins et al, ICLR, 2017)."""
+
+    def __init__(self, z_dim=64, nc=3):
+        super(BetaVAE_H, self).__init__()
+        self.z_dim = z_dim
+        self.nc = nc
+        self.encoder = nn.Sequential(
+            nn.Conv2d(nc, 32, 4, 2, 1),          # B,  32, 32, 32
+            nn.ReLU(True),
+            nn.Conv2d(32, 32, 4, 2, 1),          # B,  32, 16, 16
+            nn.ReLU(True),
+            nn.Conv2d(32, 64, 4, 2, 1),          # B,  64,  8,  8
+            nn.ReLU(True),
+            nn.Conv2d(64, 256, 4, 1),            # B, 256,  1,  1
+            nn.ReLU(True),
+            View((-1, 256*1*1)),                 # B, 256
+            nn.Linear(256, z_dim*2),             # B, z_dim*2
+        )
+        self.weight_init()
+
+    def weight_init(self):
+        for block in self._modules:
+            for m in self._modules[block]:
+                kaiming_init(m)
+
+    def forward(self, x):
+        distributions = self._encode(x)
+        mu = distributions[:, :self.z_dim]
+        logvar = distributions[:, self.z_dim:]
+        z = reparametrize(mu, logvar)  # torch.Size([32, 64])
+        kld, _, _ = kl_divergence(mu, logvar)
+        return z, mu, logvar, kld
+
+    def _encode(self, x):
+        return self.encoder(x)
+
+
+
+def kaiming_init(m):
+    if isinstance(m, (nn.Linear, nn.Conv2d)):
+        init.kaiming_normal(m.weight)
+        if m.bias is not None:
+            m.bias.data.fill_(0)
+    elif isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d)):
+        m.weight.data.fill_(1)
+        if m.bias is not None:
+            m.bias.data.fill_(0)
+
+
+def normal_init(m, mean, std):
+    if isinstance(m, (nn.Linear, nn.Conv2d)):
+        m.weight.data.normal_(mean, std)
+        if m.bias.data is not None:
+            m.bias.data.zero_()
+    elif isinstance(m, (nn.BatchNorm2d, nn.BatchNorm1d)):
+        m.weight.data.fill_(1)
+        if m.bias.data is not None:
+            m.bias.data.zero_()
+
+
+if __name__ == '__main__':
+    pass
---
+++ models/vqvae_2_cat.py
@@ -0,0 +1,102 @@
+
+import torch
+import torch.nn as nn
+import numpy as np
+from models.encoder import Encoder
+from models.quantizer import VectorQuantizer
+from models.decoder import Decoder
+from models.betavae import BetaVAE_H
+
+class VQVAE(nn.Module):
+    def __init__(self, h_dim, res_h_dim, n_res_layers,
+                 n_embeddings, embedding_dim, beta, save_img_embedding_map=False):
+        super(VQVAE, self).__init__()
+        # MI tools 
+        device = 'cuda:0'
+        sigma=0.4
+        num_bins=64
+        normalize=True
+        self.sigma = 2*sigma**2
+        self.num_bins = num_bins
+        self.normalize = normalize
+        self.epsilon = 1e-10
+        self.bins = nn.Parameter(torch.linspace(0, 255, num_bins, device=device).float(), requires_grad=False)
+
+        # encode image into continuous latent space
+        self.encoder = Encoder(3, h_dim, n_res_layers, res_h_dim)
+        self.encoder_beta = BetaVAE_H()
+        self.pre_quantization_conv = nn.Conv2d(
+            h_dim, embedding_dim, kernel_size=1, stride=1)
+        # pass continuous latent vector through discretization bottleneck
+        self.vector_quantization = VectorQuantizer(
+            n_embeddings, embedding_dim, beta)
+        # decode the discrete latent representation
+        # self.decoder = Decoder(embedding_dim, h_dim, n_res_layers, res_h_dim)
+        self.decoder = Decoder(2*embedding_dim, h_dim, n_res_layers, 2*res_h_dim)
+
+        if save_img_embedding_map:
+            self.img_to_embedding_map = {i: [] for i in range(n_embeddings)}
+        else:
+            self.img_to_embedding_map = None
+
+    def forward(self, x, verbose=False):
+        
+        z_e = self.encoder(x)
+
+        z_e = self.pre_quantization_conv(z_e)  # h_dim - embedding_dim 128 - 64
+        embedding_loss, z_q, perplexity, _, _ = self.vector_quantization(
+            z_e)
+        z_beta, _, _, kld_loss = self.encoder_beta(x)  # z：torch.Size([32, 64, 8, 8])
+        total_z_q = torch.cat((z_q, z_beta), dim=1)
+        #total_z_q = z_q + z_beta
+        MI = self.getMutualInformation(z_q, z_beta)
+        x_hat = self.decoder(total_z_q)
+
+        if verbose:
+            print('original data shape:', x.shape)
+            print('encoded data shape:', z_e.shape)
+            print('recon data shape:', x_hat.shape)
+            assert False
+
+        return embedding_loss, x_hat, perplexity, kld_loss, MI
+
+    def marginalPdf(self, values):
+        residuals = values - self.bins.unsqueeze(0).unsqueeze(0)
+        kernel_values = torch.exp(-0.5*(residuals / self.sigma).pow(2))
+        pdf = torch.mean(kernel_values, dim=1)
+        normalization = torch.sum(pdf, dim=1).unsqueeze(1) + self.epsilon
+        pdf = pdf / normalization
+        return pdf, kernel_values
+    
+    def jointPdf(self, kernel_values1, kernel_values2):
+        joint_kernel_values = torch.matmul(kernel_values1.transpose(1, 2), kernel_values2)
+        normalization = torch.sum(joint_kernel_values, dim=(1,2)).view(-1, 1, 1) + self.epsilon
+        pdf = joint_kernel_values / normalization
+        return pdf
+        
+    def getMutualInformation(self, input1, input2):
+
+        # Torch tensors for images between (0, 1)
+        input1 = input1*255
+        input2 = input2*255
+        B, C, H, W = input1.shape
+        assert((input1.shape == input2.shape))
+
+        x1 = input1.view(B, H*W, C)
+        x2 = input2.view(B, H*W, C)
+        
+        pdf_x1, kernel_values1 = self.marginalPdf(x1)
+        pdf_x2, kernel_values2 = self.marginalPdf(x2)
+        pdf_x1x2 = self.jointPdf(kernel_values1, kernel_values2)
+
+        H_x1 = -torch.sum(pdf_x1*torch.log2(pdf_x1 + self.epsilon), dim=1)
+        H_x2 = -torch.sum(pdf_x2*torch.log2(pdf_x2 + self.epsilon), dim=1)
+        H_x1x2 = -torch.sum(pdf_x1x2*torch.log2(pdf_x1x2 + self.epsilon), dim=(1,2))
+
+        mutual_information = H_x1 + H_x2 - H_x1x2
+        
+        if self.normalize:
+            mutual_information = 2*mutual_information/(H_x1+H_x2)
+
+        return mutual_information
+
